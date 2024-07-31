ad3d412d3d8c83d61cfa7efe9e0bfb915872f42a
Saravana Kumar B <saravanakumar.balasubramaniyam@kovai.co>
2024-07-31 15:07:21 +0530

    navbar scss added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { AuthService } from '../../services';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [AvatarModule, AvatarGroupModule,CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  currentRoute = "";
+  isDropdownVisible = false;
 
+  constructor(private router: Router,private authService: AuthService) {}
+
+  ngOnInit() {
+    this.currentRoute = this.router.url;
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  closeDropdown()
+  {
+    this.isDropdownVisible = false;
+  }
+
+  logout() {
+    console.log('User logged out');
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  navigateToDashboard()
+  {
+    this.router.navigate(['/dashboard']);
+  }
 }
