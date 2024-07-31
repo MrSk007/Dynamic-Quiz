c56ae90315221140da251c663b19f17acd7be21c
Saravana Kumar B <saravanakumar.balasubramaniyam@kovai.co>
2024-07-31 17:18:14 +0530

    auth details updated

--- src/app/constants/storage-key.const.ts
+++ src/app/constants/storage-key.const.ts
@@ -1,5 +1,5 @@
 export const TEST_RESULT_KEY = "results";
 export const AUTH_KEY = "authkey";
 
-export const USER_NAME = "JohnDoe1";
+export const USER_NAME = "SARAVANA007";
 export const USER_PWD = "$$pwd0"
